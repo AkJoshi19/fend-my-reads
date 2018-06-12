ecd0158cd8c486305abd72625ff1ec7718ba982e
maevanapcontact <maevanap.contact@gmail.com>
2018-06-12 16:47:52 +0200

    feat: build the Books component

--- my-reads/src/Books.js
+++ my-reads/src/Books.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react'
 
 class Books extends Component {
 	render() {
-		return <ol>
-			{this.props.books.map((book) => (
-				<li key={book.id}>
-					{book.title}
-				</li>
-			))}
-		</ol>
+		return (
+			// Complete list of books
+			<ul className='books-list'>
+				{this.props.books.map((book) => (
+					// One book item
+					<li key={book.id} className='book-item'>
+						<h2>{book.title}</h2>
+						<h3>{book.author}</h3>
+						<div className='book-info'>
+							<img src={book.cover} alt={book.title} />
+							<div className='book-btns'>
+								<button>Currently Reading</button>
+								<button>Wants to read</button>
+								<button>Read</button>
+								<button>None</button>
+							</div>
+						</div>
+					</li>
+				))}
+			</ul>
+		)
 	}
 }
 
